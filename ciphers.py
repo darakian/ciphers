b6357b1602a9486ef9d528a9c9cdd10593e52e92
Jon <jmoroney@hawaii.edu>
2016-02-27 13:00:08 -1000

    Fixed index out of bounds issue

--- ciphers.py
+++ ciphers.py
@@ -12,14 +12,18 @@ import string
 #punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 #whitespace = ' \t\n\r\x0b\x0c'
 
-def shift(cipherText="crackme",key=26,alphabet=list(string.ascii_lowercase)):
+def shift(cipherText="mbkmuwo",key=16,alphabet=list(string.ascii_lowercase)):
     key = key%len(alphabet)
     print("Shifting {} by {}".format(cipherText, key))
     result = ""
     for character in cipherText:
-        #print(alphabet[alphabet.index(character)+key])
-        result=result+(alphabet[alphabet.index(character)+key])
+        #print(alphabet[((alphabet.index(character)+key)%len(alphabet))])
+        result=result+(alphabet[((alphabet.index(character)+key)%len(alphabet))])
     print("Result: {}".format(result))
+
+def affine(cipherText="crackme",key=26,alphabet=list(string.ascii_lowercase)):
+    print("lol")
+
 
 #Testing area
 shift()
